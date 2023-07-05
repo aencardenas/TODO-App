578026b8ce440c5d69fd9d5308076086198fce7b
Andres Cardenas <aencardenas@gmail.com>
2023-07-05 17:30:08 -0600

    clase03 completada

--- script.js
+++ script.js
@@ -3,7 +3,20 @@ const btn = document.querySelector("[data-form-btn]");
 const createTask = (evento) => { /*=> arrow functions*/
     evento.preventDefault();
     const input = document.querySelector("[data-form-input]");
-    console.log(input.value);
+    const value = input.value;
+    const list = document.querySelector("[data-list]");
+    const task = document.createElement('li'); //crear una tarea
+    task.classList.add("card") //asignarle la clase a la tarea para que tenga los estilos del css
+    input.value = ''
+    //backticks
+    const content = 
+        `<div>
+            <i class="far fa-check-square icon"></i>
+            <span class="task">${value}</span>
+        </div>
+            <i class="fas fa-trash-alt trashIcon icon"></i>`;
+    task.innerHTML  = content;
+    list.appendChild(task);
 }
 
 //Listener
