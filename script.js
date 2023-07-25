ad70f71db59b18533ada7790ddd40903f8d9e7a9
Andres Cardenas <aencardenas@gmail.com>
2023-07-24 19:20:45 -0600

    feat: script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+( () => {
+    
 const btn = document.querySelector("[data-form-btn]");
 
 const createTask = (evento) => { /*=> arrow functions*/
@@ -9,15 +11,47 @@ const createTask = (evento) => { /*=> arrow functions*/
     task.classList.add("card") //asignarle la clase a la tarea para que tenga los estilos del css
     input.value = ''
     //backticks
-    const content = 
-        `<div>
-            <i class="far fa-check-square icon"></i>
-            <span class="task">${value}</span>
-        </div>
-            <i class="fas fa-trash-alt trashIcon icon"></i>`;
-    task.innerHTML  = content;
+    const taskContent = document.createElement("div");
+    taskContent.appendChild(checkComplete());
+    const titleTask = document.createElement("span");
+    titleTask.classList.add("task");
+    titleTask.innerText = value;
+    taskContent.appendChild(titleTask)
+    
+    //task.innerHTML  = content;S
+    task.appendChild(taskContent)
+    task.appendChild(deleteIcon())
     list.appendChild(task);
 }
 
 //Listener
-btn.addEventListener("click", createTask)
+btn.addEventListener("click", createTask);
+
+const checkComplete = () => {
+    const i = document.createElement("i");
+    i.classList.add("far","fa-check-square","icon");
+    i.addEventListener("click",completeTask)
+    return i;
+}
+
+//Inmediately invoked function expression IIFE
+const completeTask = (event) => {
+    const element = event.target;
+    element.classList.toggle("fas");
+    element.classList.toggle("completeIcon");
+    element.classList.toggle("far");
+
+}
+
+const deleteIcon = () =>{
+    const i = document.createElement("i");
+    i.classList.add("fas", "fa-trash-alt", "trashIcon", "icon");
+    i.addEventListener("click", deleteTask)
+    return i;
+}
+
+const deleteTask = (event) => {
+    const parent = event.target.parentElement;
+    parent.remove();
+}
+})();
